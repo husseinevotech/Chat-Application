743efa62c9556dfcc72477531de2f6edb362630c
Hussien EvoTech <h.ali@evotech-co.com>
2021-12-26 13:15:41 +0200

    chat ui

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MessageController extends Controller
+{
+    public function conversation($userId) {
+        $users = User::where('id', '!=', Auth::id())->get();
+        $friendInfo = User::findOrFail($userId);
+        $myInfo = User::find(Auth::id());
+        // $groups = MessageGroup::get();
+
+        $this->data['users'] = $users;
+        $this->data['friendInfo'] = $friendInfo;
+        $this->data['myInfo'] = $myInfo;
+        $this->data['users'] = $users;
+        // $this->data['groups'] = $groups;
+
+        return view('message.conversation', $this->data);
+    }
+}
---
+++ resources/views/message/conversation.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-3">
+            <div class="users">
+                <h5>Users</h5>
+
+                <ul class="list-group list-chat-item">
+                    @if($users->count())
+                        @foreach($users as $user)
+                            <li class="chat-user-list @if($user->id == $friendInfo->id) active @endif">
+                                <a href="{{ route('message.conversation', $user->id) }}">
+                                    <div class="chat-image">
+                                            {!! makeImageFromName($user->name) !!}
+                                        <i class="fa fa-circle user-status-icon user-icon-{{ $user->id }}" title="away"></i>
+                                    </div>
+
+                                    <div class="chat-name font-weight-bold">
+                                        {{ $user->name }}
+                                    </div>
+                                </a>
+                            </li>
+                        @endforeach
+                    @endif
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-9 chat-section">
+            <div class="chat-header">
+                <div class="chat-image">
+                    {!! makeImageFromName($user->name) !!}
+                </div>
+
+                <div class="chat-name font-weight-bold">
+                    {{ $user->name }}
+                    <i class="fa fa-circle user-status-head" title="away"
+                     id="userStatusHead{{ $friendInfo->id }}"></i>
+                </div>
+            </div>
+
+            <div class="chat-body" id="chatBody">
+                <div class="message-listing" id="messageWrapper">
+                    <div class="row message align-item-center mb-2">
+                        <div class="col-md-12 user-info">
+                            <div class="chat-image">
+                                {!! makeImageFromName("User Name") !!}
+                            </div>
+
+                            <div class="chat-name font-weight-bold">
+                                User name
+                                <span class="small time text-gray-500" title="2020-05-06 10:30 PM">
+                                    10:30 pm
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12 message-content">
+                            <div class="message-text">
+                                Message here
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat-box">
+                <div class="chat-input bg-white" id="chatInput" contenteditable="">
+
+                </div>
+
+                <div class="chat-input-toolbar">
+                    <button title="Add File" class="btn btn-light btn-sm btn-file-upload">
+                        <i class="fa fa-paperclip"></i>
+                    </button> |
+
+                    <button title="Bold" class="btn btn-light btn-sm tool-items"
+                            onclick="document.execCommand('bold', false, '');">
+                        <i class="fa fa-bold tool-icon"></i>
+                    </button>
+
+                    <button title="Italic" class="btn btn-light btn-sm tool-items"
+                            onclick="document.execCommand('italic', false, '');">
+                        <i class="fa fa-italic tool-icon"></i>
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
+
